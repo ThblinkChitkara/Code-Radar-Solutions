6898878982a04e99888cc213f2b11efed7301f99
ThblinkChitkara <theobald1009.cse24@chitkara.edu.in>
2025-01-22 09:08:00 +0530

    Solution to Check Greater Than or Equal To

--- Check Greater Than or Equal To@@67882a731b4392d8fc1577d8/code.c
+++ Check Greater Than or Equal To@@67882a731b4392d8fc1577d8/code.c
@@ -3,8 +3,8 @@
 
 
 int main(int argc, char** argv) {
-    long a = atoi(argv[1]);
-    long b = atoi(argv[2]);
+    long a = argv[1];
+    long b = argv[2];
     char* rep = (a >= b) ? "True" : "False";
     return 0;
 }
