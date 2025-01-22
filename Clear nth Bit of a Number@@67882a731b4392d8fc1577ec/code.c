fb8e9d5c3fac08f067c65a26a9d3d93440f2c8bc
ThblinkChitkara <theobald1009.cse24@chitkara.edu.in>
2025-01-22 10:27:59 +0530

    Solution to Clear nth Bit of a Number

--- Clear nth Bit of a Number@@67882a731b4392d8fc1577ec/code.c
+++ Clear nth Bit of a Number@@67882a731b4392d8fc1577ec/code.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
 
+
+unsigned int reverseBits(unsigned int num) {
+    // Étape 1 : Échange des moitiés des bits
+    num = ((num & 0xFFFF0000) >> 16) | ((num & 0x0000FFFF) << 16);
+    // Étape 2 : Échange des blocs de 8 bits
+    num = ((num & 0xFF00FF00) >> 8) | ((num & 0x00FF00FF) << 8);
+    // Étape 3 : Échange des blocs de 4 bits
+    num = ((num & 0xF0F0F0F0) >> 4) | ((num & 0x0F0F0F0F) << 4);
+    // Étape 4 : Échange des blocs de 2 bits
+    num = ((num & 0xCCCCCCCC) >> 2) | ((num & 0x33333333) << 2);
+    // Étape 5 : Échange des bits individuels
+    num = ((num & 0xAAAAAAAA) >> 1) | ((num & 0x55555555) << 1);
+
+    return num;
+}
+
 int main() {
     int a, b, c;
     scanf("%i", &a);
     scanf("%i", &b);
     c = 1;
     c <<= b;
-    a ^= b;
-    
+    c = reverseBits(c);
+    a &= c;
     printf("%i", a);
     return 0;
 }
