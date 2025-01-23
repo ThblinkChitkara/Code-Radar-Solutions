a27158eb8d1c99a7e9d6574380df5f7adfde546b
ThblinkChitkara <theobald1009.cse24@chitkara.edu.in>
2025-01-23 15:18:24 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -1,26 +1,51 @@
 #include <stdio.h>
 
 char* vowel_or_consonant(char a){
-    return switch a {
+    switch a {
         case "a":
-            "Vowel";
+            return "Vowel";
         case "e":
-            "Vowel";
+            return "Vowel";
         case "i":
-            "Vowel";
+            return "Vowel";;
         case "o":
-            "Vowel";
+            return "Vowel";
         case "u":
-            "Vowel";
+            return "Vowel";
         case "y":
-            "Vowel";
+            return "Vowel";
         default :
-            "Consonant";
+            return "Consonant";
+    }
+}
+
+char* vowel_or_consonant_MAJ(char a){
+    switch a {
+        case "a":
+            return "Vowel";
+        case "e":
+            return "Vowel";
+        case "i":
+            return "Vowel";;
+        case "o":
+            return "Vowel";
+        case "u":
+            return "Vowel";
+        case "y":
+            return "Vowel";
+        default :
+            return "Consonant";
     }
 }
 
 int main() {
     char a;
-    printf("s");
+    scanf("%c", &a);
+
+    printf("%s", 
+    (a >= 'a' && a <= 'z') ? vowel_or_consonant(a) :
+    (a >= 'A' && a <= 'Z') ? vowel_or_consonant_MAJ(a) :
+    (a >= '0' && a <= '9') ? "Digit" : "Special Character"
+    );
     return 0;
 }
