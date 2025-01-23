636c910c02cf59771a3147dc844d04d7ad92815a
ThblinkChitkara <theobald1009.cse24@chitkara.edu.in>
2025-01-23 15:11:42 +0530

    Solution to Check Character Type

---
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+char* vowel_or_consonant(char a){
+    return switch a {
+        case "a":
+            "Vowel";
+        case "e":
+            "Vowel";
+        case "i":
+            "Vowel";
+        case "o":
+            "Vowel";
+        case "u":
+            "Vowel";
+        case "y":
+            "Vowel";
+        default :
+            "Consonant";
+    }
+}
+
+int main() {
+    char a;
+    printf("%s", welcome());
+    return 0;
+}
