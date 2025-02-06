26204d3e66b5f62889fb508ccb0fca588871db92
ThblinkChitkara <theobald1009.cse24@chitkara.edu.in>
2025-02-06 15:06:11 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    char* a;
+    scanf("%s", &a);
+    printf(strcmp(a, "A") ? "Excellent" : strcmp(a, "B") ? "Good" : strcmp(a, "C") ? "Average" :
+    strcmp(a, "D") ? "Below Average" : strcmp(a, "F") ? "Fail" : "Invalid grade");
+    return 0;
+}
