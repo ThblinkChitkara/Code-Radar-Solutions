d13c740d394cab4fdc2c90da6cbe752b0afa9ce5
ThblinkChitkara <theobald1009.cse24@chitkara.edu.in>
2025-02-07 14:20:47 +0530

    Solution to Grade Evaluation

--- Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -4,6 +4,8 @@ include <string.h>
 int main() {
     char a;
     scanf("%c", &a);
-    printf((a == 'A') ? "Excellent" : (a == 'B') ? "Good" : (a == 'C') ? "Average" : (a == 'D') ? "Below Average" : (a == 'F') ? "Fail" : "Invalid grade");
+    printf((a == 'A') ? "Excellent" : (a == 'B') 
+    ? "Good" : (a == 'C') ? "Average" : (a == 'D') 
+    ? "Below Average" : (a == 'F') ? "Fail" : "Invalid grade");
     return 0;
 }
