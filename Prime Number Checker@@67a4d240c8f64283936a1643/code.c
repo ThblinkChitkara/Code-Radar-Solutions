ec86149de1c7e74b78da2e1f78af8e0e72030153
Theobald Alfred Marie Maurice <theobald1009.cse24@chitkara.edu.in>
2025-03-10 15:58:35 +0530

    Solution to Prime Number Checker

--- Prime Number Checker@@67a4d240c8f64283936a1643/code.c
+++ Prime Number Checker@@67a4d240c8f64283936a1643/code.c
@@ -1,8 +1,12 @@
-int isPrime(int a){
-    int rep = 1;
-   
-    for (int i = 2; i < a/2 && rep; i++){
-        rep = (a%i != 0);
+#include <math.h>
+
+int isPrime(int a) {
+    if (a < 2) return 0; // 0 et 1 ne sont pas premiers
+    if (a == 2) return 1; // 2 est le seul nombre premier pair
+    if (a % 2 == 0) return 0; // Élimination des nombres pairs
+
+    for (int i = 3; i <= sqrt(a); i += 2) {
+        if (a % i == 0) return 0; // Si divisible, ce n'est pas un nombre premier
     }
-    return rep;
+    return 1; // Si aucun diviseur trouvé, alors premier
 }
