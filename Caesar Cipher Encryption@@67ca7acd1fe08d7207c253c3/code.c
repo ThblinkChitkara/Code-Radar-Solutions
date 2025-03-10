f0b92504685b2f66298dbf1f158b837dbb91fbc1
Theobald Alfred Marie Maurice <theobald1009.cse24@chitkara.edu.in>
2025-03-10 22:57:26 +0530

    Solution to Caesar Cipher Encryption

---
+++ Caesar Cipher Encryption@@67ca7acd1fe08d7207c253c3/code.c
@@ -0,0 +1,16 @@
+#include <ctype.h>
+
+// Fonction pour chiffrer le message avec le chiffrement de César
+void caesarCipher(char message[], int shift, char encrypted[]) {
+    for (int i = 0; message[i] != '\0'; i++) {
+        char c = message[i];
+
+        if (isalpha(c)) {
+            char base = isupper(c) ? 'A' : 'a';
+            encrypted[i] = (c - base + shift) % 26 + base;
+        } else {
+            encrypted[i] = c; // Ne change pas les caractères non alphabétiques
+        }
+    }
+    encrypted[strlen(message)] = '\0'; // Ajouter le caractère de fin de chaîne
+}
